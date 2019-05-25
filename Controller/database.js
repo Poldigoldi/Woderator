cf7ab74ed2fc3738e955fcd011f41081d3aaaccf
Leonard Goldschmidt <leo.goldschmidt@yellowdog.cot>
2019-05-25 10:24:02 +0100

    Implemeted duration check in database.js

--- Controller/database.js
+++ Controller/database.js
@@ -22,7 +22,19 @@ module.exports = {
 		try {
 			var sql = 'SELECT * FROM Workouts WHERE muscleGroup = ? AND type = ?';
 			con.query(sql, [req.params.MuscleGroup, req.params.Type], await function (err, result) {
-		    	res.render('workout', { MuscleGroup: req.params.MuscleGroup, Duration: req.params.Duration, EXERCISE: result });
+				var workout = [];
+				var workoutDuration = 0;
+				var desiredDuration = req.params.Duration;
+
+				//Builds up workout cards for desired duration
+				for (var i = result.length - 1; i >= 0; i--) {
+					workoutDuration = workoutDuration + result[i].duration;
+					if (workoutDuration <= desiredDuration) {
+						workout.push(result[i]);
+					}
+				}
+
+		    	res.render('workout', { MuscleGroup: req.params.MuscleGroup, Duration: req.params.Duration, EXERCISE: workout });
 	  		});
 		} catch {
 			console.log("getWorkout ERROR: cannot execute query!")
@@ -32,7 +44,7 @@ module.exports = {
 	async submitWorkout(req, res) {
 		try {
 			var MuscleGroup = await req.body.MuscleGroup;
-			var Duration = await req.body.Muration;
+			var Duration = await req.body.Duration;
 			var Type = await req.body.Type;
 	  		await res.redirect('/workout/' + MuscleGroup + '/' + Duration + '/' + Type);
 		} catch {
