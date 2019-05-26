ada4b2bb694f8e3f6315aae0668b43b79dc30aef
Leonard Goldschmidt <leo.goldschmidt@yellowdog.cot>
2019-05-26 18:46:51 +0100

    Added remove from favourites and some animations

--- routes/routes.js
+++ routes/routes.js
@@ -9,5 +9,7 @@ router.post('/workout/submit', dbController.submitWorkout);
 router.get('/workout/:MuscleGroup/:Duration/:Type', dbController.getWorkout);
 router.post('/favourites', dbController.saveFavourite);
 router.get('/favourites', dbController.getFavourites);
+router.delete('/favourites/remove/:favouriteID', dbController.removeFavourite);
+
 
 module.exports = router;
